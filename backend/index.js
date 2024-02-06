44af00849abdd944b6c362bca29493ba2184c92d
OU5AMA <lasri.oussama@gmail.com>
2024-02-06 14:26:55 +0100

    add middwares; express.json(), cors()

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,16 @@ const cors = require("cors");
 
 
 
+app.use(express.json());
+app.use(cors());
 
-
-// app.get('/', (req, res)=>{
-//     try {
-//         res.status(200).json({"message": "you're on the UI"});
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
+app.get('/', (req, res)=>{
+    try {
+        res.status(200).json({"message": "you're on the UI"});
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 
