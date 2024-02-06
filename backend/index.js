e892f76a30ce0a84f5323e36b1d8875f12628980
OU5AMA <lasri.oussama@gmail.com>
2024-02-06 14:23:09 +0100

    install mongoDB, mongoose, nodemon, express, cors, dotenv. listen to port 3000

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+
+
+
+// app.get('/', (req, res)=>{
+//     try {
+//         res.status(200).json({"message": "you're on the UI"});
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("you're listening to port 3000, SUCCESSFULLY")
+})
