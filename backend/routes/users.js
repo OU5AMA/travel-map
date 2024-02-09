07a0120f7aeb466cf034903dbbb2d1f89f3b306c
OU5AMA <lasri.oussama@gmail.com>
2024-02-09 15:01:16 +0100

    add a login to user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -24,4 +24,25 @@ router.post("/register", async (res, req) => {
 })
 
 
+// login
+router.post("/login", async (req, res)=>{
+    try {
+        // find user
+        const user = await User.findOne({username})
+        !user && res.status(400).json("Wrong username or password")
+
+        // validate password
+        const validPassword = await bcrypt.compare(
+            req.body.password, 
+            user.password 
+            );
+        
+        // send response
+        res.status(200).json({_id:user._id, username: username});
+    } catch (error) {
+        
+    }
+})
+
+
 module.exports = router
